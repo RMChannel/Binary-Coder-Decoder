ce837698c5908f5ffa6c5ee7a13b199b4e7d0c18
RMChannel <98057423+RMChannel@users.noreply.github.com>
2023-06-04 12:23:10 +0200

    Create binarycoder-decoder.py

---
+++ binarycoder-decoder.py
@@ -0,0 +1,22 @@
+def frombinary(code):
+    print(True)
+    while (len(code)%8)!=0:
+        code="0"+code
+    temp=""
+    i2=0
+    for i in code:
+        if (i2<8):
+            temp=temp+i
+            i2+=1
+        else:
+            list.append(temp)
+            temp=i
+            i2=1
+    list.append(temp)
+    result=""
+    for i in list:
+        result=result+(chr(int(i,2)))
+    return result
+
+def tobinary(text):
+    return (''.join(format(ord(i), '08b') for i in text)).replace(" ","")[1:]
